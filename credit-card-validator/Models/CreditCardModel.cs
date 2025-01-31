883410dc427d215732073488c32ae28033d53389
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-01-31 21:06:35 +0530

    created the basivc struct of generic credit card

---
+++ credit-card-validator/Models/CreditCardModel.cs
@@ -0,0 +1,10 @@
+﻿namespace credit_card_validator.Models;
+
+public class CreditCardModel
+{
+    public long CardNumber { set; get; }
+    public string CardHolderName { set; get; }
+    public string ExpirationDate { set; get; }
+    public int CVV { set; get; }
+    public string IssuedBank { set; get; }
+}
