0575cb5c8b9206ced37249a9fced2ba7e275f77f
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-02-01 20:52:26 +0530

    enhanced by adding regex and validations

--- credit-card-validator/Models/CreditCardModel.cs
+++ credit-card-validator/Models/CreditCardModel.cs
@@ -1,10 +1,22 @@
-﻿namespace credit_card_validator.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace credit_card_validator.Models;
 
 public class CreditCardModel
 {
-    public long CardNumber { set; get; }
-    public string CardHolderName { set; get; }
-    public string ExpirationDate { set; get; }
-    public int CVV { set; get; }
-    public string IssuedBank { set; get; }
+    [Key] public long CardNumber { set; get; }
+
+    [Required(ErrorMessage = "Please enter card holder's name")]
+    [RegularExpression(@"^(4\d{16}|3[47]\d{15}|(22|5[1-5])\d{16}|6011\d{16})$", ErrorMessage = "Invalid card number")]
+    public required string CardHolderName { set; get; }
+
+    [Required(ErrorMessage = "Please enter expiration date")]
+    public DateOnly ExpirationDate { set; get; }
+
+    [Required(ErrorMessage = "Please enter cvv value")]
+    public required int CVV { set; get; }
+
+    [Required(ErrorMessage = "Please enter card bank name")]
+    public required string IssuedBank { set; get; }
 }
