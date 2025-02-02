547946a065c01da87a5342d98756da22ac41b805
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-02-02 14:52:17 +0530

    initialized controller

---
+++ credit-card-validator/Controllers/CreditCardController.cs
@@ -0,0 +1,16 @@
+﻿using credit_card_validator.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace credit_card_validator.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CreditCardController(ILogger<CreditCardController> logger) : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Post([FromBody] CreditCardDTO cardDto)
+    {
+        logger.LogInformation("Request Received | {}", cardDto);
+        return Ok();
+    }
+}
--- credit-card-validator/Models/CreditCardModel.cs
+++ credit-card-validator/Models/CreditCardModel.cs
@@ -1,7 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data;
-using System.Runtime.InteropServices.JavaScript;
 
 namespace credit_card_validator.Models;
 
