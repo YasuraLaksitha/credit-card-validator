dd6bd294c0e4e5bf9d578854dcaa63940ff47adf
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-02-02 18:41:46 +0530

    created Database ctx

---
+++ credit-card-validator/Context/DataBaseContext.cs
@@ -0,0 +1,10 @@
+﻿using credit_card_validator.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace credit_card_validator.Context
+{
+    public class DataBaseContext(DbContextOptions<DataBaseContext> options) : DbContext(options)
+    {
+        public DbSet<CreditCardModel> CreditCards { get; set; }
+    }
+};
--- credit-card-validator/Program.cs
+++ credit-card-validator/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DbContext>(opt =>
+{
+    opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
